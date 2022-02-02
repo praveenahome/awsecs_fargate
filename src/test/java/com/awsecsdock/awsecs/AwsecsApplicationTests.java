d1f8ba12ad5db33770c8a6e7639cf1454cf8d272
pchennapareddy <praveenareddy151@gmail.com>
2022-02-02 14:52:38 +0530

    first commit in awsecs

---
+++ src/test/java/com/awsecsdock/awsecs/AwsecsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awsecsdock.awsecs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsecsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
